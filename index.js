e98717f5a77dd16ff2554759bd553fbac775b493
Saïd Redouane <s_red009@hotmail.com>
2021-02-14 02:20:07 +0000

    Refactored the code using OOP

--- index.js
+++ index.js
@@ -1,31 +1,37 @@
-// Arrays to keep track of each task's state
-const taskTitles = [];
-const taskComplete = [];
+//Function to create a new task object
+const newTask = function(title, description) {
+  const task = {
+    title: title,
+    description: description,
+    complete: false,
 
-// Create a new task by adding to the arrays
-// A new task will be created as incomplete
-function newTask(title) {
-  taskTitles.push(title);
-  taskComplete.push(false);
-}
+    // Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
+    markCompleted: function() {
+      this.complete = true;
+    },
 
-// Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-function completeTask(taskIndex) {
-  taskComplete[taskIndex] = true;
-}
+    // Print the state of a task to the console in a nice readable way
+    logState: function() {
+      const title = this.title;
+      const complete = this.complete;
+      console.log(`${title} has${complete ? " " : " not "}been completed`);
+    },
+  };
+  return task;
+};
 
-// Print the state of a task to the console in a nice readable way
-function logTaskState(taskIndex) {
-  const title = taskTitles[taskIndex];
-  const complete = taskComplete[taskIndex];
-  console.log(`${title} has${complete ? " " : " not "}been completed`);
-}
+// Test Code
 
-// DRIVER CODE BELOW
+const task1 = newTask(
+  "Clean Cat Litter",
+  "Take all the 💩 out of the litter box"
+);
+// const task2 = newTask("Do Laundry", "😨");
+// const tasks = [task1, task2];
 
-newTask("Clean Cat Litter"); // task 0
-newTask("Do Laundry"); // task 1
+// console.log(tasks);
 
-logTaskState(0); // Clean Cat Litter has not been completed
-completeTask(0);
-logTaskState(0); // Clean Cat Litter has been completed
+console.log(task1);
+task1.markCompleted();
+console.log(task1);
+task1.logState();
